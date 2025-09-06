b612568c8493b0aed91395fff52e6bea3a93f153
Elizabeth1420 <elizabethadeleke683@gmail.com>
2025-09-06 17:40:48 +0100

    added login and recommendation

--- recommendation.js
+++ recommendation.js
@@ -10,12 +10,7 @@ router.post('/recommendation', authMiddleware, async (req, res) => {
   if (!work_id || !rating || !review || !title || !employee_id || !department) {
     return res.status(400).json({ error: 'All fields are required.' });
   }
-  // const { data: user, error: userError } = await supabase.auth.getUser();
-  // if (userError) {
-  //   console.log(userError)
-  //   return res.status(400).json({ error: userError.message });
-  // }
- 
+   
   const { data, error } = await supabase
     .from('recommendations')
     .insert([{ 
